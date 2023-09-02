6e9f598a26db76f732b529f2716c2a715a2c2954
Daniel Shijakovski <daniel.shijakovski@testdevlab.com>
2023-09-02 14:30:14 +0200

    bubble sort - part 4

--- Day 4/construct_algorithm/4_bubble_sort.js
+++ Day 4/construct_algorithm/4_bubble_sort.js
@@ -2,9 +2,10 @@
 
 // ! Let's get our trusty list back in here
 let myList = [4, 2, 6, 2, 8, 0, 1, 5]
+let len = myList.length
 
 // ! Quick recap, this is what we have made so far
-for (let i = 0; i < myList.length - 1; i++) {
+for (let i = 0; i < len - 1; i++) {
 	if (myList[i] > myList[i + 1]) {
 		// Swap
 		let temp = myList[i]
@@ -14,7 +15,7 @@ for (let i = 0; i < myList.length - 1; i++) {
 }
 
 // ! After the loop, this is how the array looks like
-// console.log(myList) // ! [2, 4, 2, 6, 0, 1, 5, 8] -> the 8 is at the end of the array
+console.log(myList) // ! [2, 4, 2, 6, 0, 1, 5, 8] -> the 8 is at the end of the array
 
 // * So, we have managed to sort ONE element correctly
 // ! What we need next is just to DO THAT MULTIPLE TIMES
@@ -27,9 +28,9 @@ for (let i = 0; i < myList.length - 1; i++) {
 // ! Well, let's do that, take the above algorithm, and wrap it in a loop
 
 // Outer loop
-for (let k = 0; k < myList.length; k++) {
+for (let k = 0; k < len; k++) {
 	// Our algorithm from before - sorts 1 element correctly
-	for (let i = 0; i < myList.length - 1; i++) {
+	for (let i = 0; i < len - 1; i++) {
 		if (myList[i] > myList[i + 1]) {
 			// Swap
 			let temp = myList[i]
@@ -43,10 +44,10 @@ for (let k = 0; k < myList.length; k++) {
 console.log(myList)
 
 // ! Hurray 🎉
-// ! We have successfully sorted our array! (no rhyme intended)
-// ! That's it really, that's how the bubble sort algorithm works!
+// ! We have successfully sorted our array!
+// ! That's it really, that's how the Bubble Sort algorithm works!
 
 // ! We can end it here and stick with this algorithm
 // ! OR we can try to optimize it a little bit
 
-// ! Go to '5_optimization_outer.js' next
+// ? Go to '5_optimization_outer.js' next
