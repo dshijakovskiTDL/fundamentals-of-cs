2ff680ad53e9c47b6c6d35cd6176b5ab3de2d7fb
Daniel Shijakovski <daniel.shijakovski@testdevlab.com>
2024-03-03 00:33:05 +0100

    Update conditionals examples

--- Day 2/control_flow/1. conditionals/else_if/else_if.js
+++ Day 2/control_flow/1. conditionals/else_if/else_if.js
@@ -1,11 +1,11 @@
-age = 10
+let age = 10
 
 if (age < 12) {
-	console.log('You are a little kid!')
+	console.log('You are a little kid! 0-11')
 } else if (age < 18) {
-	console.log('You are a teenager!')
+	console.log('You are a teenager! 12-17')
 } else if (age < 30) {
-	console.log('You are in your Golden Years, or so old people say!')
+	console.log('You are in your Golden Years, or so old people say! 18-29')
 } else {
-	console.log('You can now complain about the new music and be right!')
+	console.log('You can now complain about the new music and be right! 30-RIP')
 }
--- Day 2/control_flow/1. conditionals/else_if/else_if.py
+++ Day 2/control_flow/1. conditionals/else_if/else_if.py
@@ -1,10 +1,10 @@
 age = 10
 
 if age < 12:
-    print("You are a little kid!")
+    print("You are a little kid! 0-11")
 elif age < 18:
-    print("You are a teenager!")
+    print("You are a teenager! 12-17")
 elif age < 30:
-    print("You are in your 'Golden Years', or so old people say!")
+    print("You are in your 'Golden Years', or so old people say! 18-29")
 else:
-    print("You can now complain about the new music and be right!")
+    print("You can now complain about the new music and be right! 30-RIP")
