356d1f2fcef2267969b5c3bc921813030dc0a603
Daniel Shijakovski <daniel.shijakovski@testdevlab.com>
2024-03-06 14:45:45 +0100

    Update value comments

--- Day 2/control_flow/3.1 functions_args/value/value.js
+++ Day 2/control_flow/3.1 functions_args/value/value.js
@@ -1,6 +1,7 @@
 // * Pass by value
 
 function doSomething(x) {
+  // This 'x' is a COPY of the 'x' variable outside of the function
   x = 100;
   console.log("x inside the function:", x);
 }
@@ -8,5 +9,3 @@ function doSomething(x) {
 x = 10;
 doSomething(x);
 console.log("x outside the function:", x);
-
-// ? It doesn't matter if they are the same name or not
--- Day 2/control_flow/3.1 functions_args/value/value.py
+++ Day 2/control_flow/3.1 functions_args/value/value.py
@@ -1,6 +1,7 @@
 # Pass by value
 
 def do_something(x):
+    # This 'x' is a COPY of the 'x' variable outside of the function
     x = 100
     print("x inside the function:", x)
 
@@ -8,5 +9,3 @@ def do_something(x):
 x = 10
 do_something(x)
 print("x outside the function:", x)
-
-# ? It doesn't matter if they are the same name or not
