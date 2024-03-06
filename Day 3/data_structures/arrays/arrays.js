be5cc7f08efeec9948914d6fc9b7d0b89664aa98
Daniel Shijakovski <daniel.shijakovski@testdevlab.com>
2024-03-06 14:40:47 +0100

    Update arrays comments

--- Day 3/data_structures/arrays/arrays.js
+++ Day 3/data_structures/arrays/arrays.js
@@ -1,7 +1,7 @@
-let numbers = [1, 2, 3, 4, 5];
+let numbers = [10, 20, 30, 40, 50];
 
-console.log(numbers[0]); // The first (0 - indexed) element
-console.log(numbers[1]); // The second (1 - indexed) element
-console.log(numbers[2]); // The third (2 - indexed) element
-console.log(numbers[3]); // The fourth (3 - indexed) element
-console.log(numbers[4]); // The fifth (4 - indexed) element
+console.log(numbers[0]); // "10": The first (0 - indexed) element
+console.log(numbers[1]); // "20": The second (1 - indexed) element
+console.log(numbers[2]); // "30": The third (2 - indexed) element
+console.log(numbers[3]); // "40": The fourth (3 - indexed) element
+console.log(numbers[4]); // "50": The fifth (4 - indexed) element
--- Day 3/data_structures/arrays/arrays.py
+++ Day 3/data_structures/arrays/arrays.py
@@ -1,7 +1,7 @@
-numbers = [1, 2, 3, 4, 5]
+numbers = [10, 20, 30, 40, 50]
 
-print(numbers[0]) # The first (0 - indexed) element
-print(numbers[1]) # The second (1 - indexed) element
-print(numbers[2]) # The third (2 - indexed) element
-print(numbers[3]) # The fourth (3 - indexed) element
-print(numbers[4]) # The fifth (4 - indexed) element
+print(numbers[0])  # "10": The first (0 - indexed) element
+print(numbers[1])  # "20": The second (1 - indexed) element
+print(numbers[2])  # "30": The third (2 - indexed) element
+print(numbers[3])  # "40": The fourth (3 - indexed) element
+print(numbers[4])  # "50": The fifth (4 - indexed) element
